982cd07adf7f5aa8b53be870e9d67fa691922451
Ma Yi <mayi@urplus.cn>
2023-11-08 14:38:41 +0800

    Add quantized models in enum WhisperModel.

--- lib/download_model.dart
+++ lib/download_model.dart
@@ -7,29 +7,56 @@ enum WhisperModel {
   /// tiny model for all languages
   tiny('tiny'),
 
+  /// tiny quantized model for all languages
+  tinyQ5('tiny-q5_1'),
+
   /// base model for all languages
   base('base'),
+
+  /// base quantized model for all languages
+  baseQ5('base-q5_1'),
 
   /// small model for all languages
   small('small'),
 
+  /// small quantized model for all languages
+  smallQ5('small-q5_1'),
+
   /// medium model for all languages
   medium('medium'),
+
+  /// medium quantized model for all languages
+  mediumQ5('medium-q5_0'),
 
   /// large model for all languages
   large('large'),
 
+  /// large quantized model for all languages
+  largeQ5('large-q5_0'),
+
   /// tiny model for english only
   tinyEn('tiny.en'),
+
+  /// tiny quantized model for english only
+  tinyEnQ5('tiny.en-q5_1'),
 
   /// base model for english only
   baseEn('base.en'),
 
+  /// base quantized model for english only
+  baseEnQ5('base.en-q5_1'),
+
   /// small model for english only
   smallEn('small.en'),
 
+  /// small quantized model for english only
+  smallEnQ5('small.en-q5_1'),
+
   /// medium model for english only
-  mediumEn('medium.en');
+  mediumEn('medium.en'),
+
+  /// medium quantized model for english only
+  mediumEnQ5('medium.en-q5_0');
 
   const WhisperModel(this.modelName);
 
